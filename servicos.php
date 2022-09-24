e669886cbdc9fa2527dc8d2105e864b8c55ec153
Policarpo <t.policarpo99@gmail.com>
2022-09-24 16:44:37 -0300

    Criada a logica para o select

--- servicos.php
+++ servicos.php
@@ -1,3 +1,20 @@
+<?php
+   include "entities/Servico.class.php";
+
+   $servico0 = new Servico("default","Selecione um tipo de serviço",0);
+   $servico1 = new Servico("Hidruáulico","Desentupimento de Teto",99.99);
+   $servico2 = new Servico("Hidruáulico","Desentupimento de Escada",85.59);
+   $servico3 = new Servico("Hidruáulico","Desentupimento de Vaso",55.70);
+   $servico4 = new Servico("Hidruáulico","Desentupimento de Torneira",45.10);
+   $servicosHidraulicos = array($servico0,$servico1,$servico2,$servico3,$servico4);
+
+   function exibirServicos($servicos) {
+    for ($i=0;$i<count($servicos);$i++) {
+        echo "<option value='$i'>".$servicos[$i]->getServico()."</option>";
+       }
+   }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -10,11 +27,9 @@
     <main>
         <form action="servicos-confirmar.php" method="post">
             <select name="servicos" id="servicos">
-                <option value="Default">Selecione um serviço</option>
-                <option value="teto">Desentupimento de Teto</option>
-                <option value="escada">Desentupimento de Escada</option>
-                <option value="vaso">Desentupimento de Vaso</option>
-                <option value="torneira">Desentupimento de Torneira</option>
+               <?php
+                exibirServicos($servicosHidraulicos);
+               ?>
             </select>
             <input type="submit" value="Confirmar Serviço">
         </form>
