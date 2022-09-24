85f8d5650abd0d6a94e03e487e46663e51ad324e
Policarpo <t.policarpo99@gmail.com>
2022-09-24 14:57:18 -0300

    Removida a classe Serviço

--- servicos-confirmar.php
+++ servicos-confirmar.php
@@ -1,29 +1,22 @@
-<?php
-    class Servico {
-        public $tipo_servico;
-        public $servico;
-        public $preco;
+<?php 
+    include "Usuario.class.php"; 
+    include "Servico.class.php"; 
 
-        public function __construct($tipo_servico, $servico, $preco){
-            $this->tipo_servico = $tipo_servico;
-            $this->servico = $servico;
-            $this->preco = $preco;
-        }
-    }
-
-    $servico1 = new Servico("Bombeiro Hidráulico", "Desentupimento de Teto", 239.99);
+    $user1 = new Usuario("Thiago","Policarpo", "tpoli@gmail.com",12345678);
+    $servico1 = new Servico("hidraulico","desentupimento de teto",385.59);
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        var_dump($user1);
         var_dump($servico1);
     ?>
 </body>
