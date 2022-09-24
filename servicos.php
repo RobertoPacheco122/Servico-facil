bcc227e6ea55a15be048f959d5cf9f9ef46691b8
Roberto Pacheco <Robertoaruajo230@hotmail.com>
2022-09-24 14:21:03 -0300

    Criando estrutura inicial do arquivo servicos.php

---
+++ servicos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serviço Fácil | Serviços</title>
+</head>
+<body>
+    <main>
+        <form action="servicos-confirmar.php" method="post">
+            <select name="servicos" id="servicos">
+                <option value="Default">Selecione um serviço</option>
+                <option value="teto">Desentupimento de Teto</option>
+                <option value="escada">Desentupimento de Escada</option>
+                <option value="vaso">Desentupimento de Vaso</option>
+                <option value="torneira">Desentupimento de Torneira</option>
+            </select>
+            <input type="submit" value="Confirmar Serviço">
+        </form>
+    </main>
+</body>
+</html>
