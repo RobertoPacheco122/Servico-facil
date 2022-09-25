362cf393ac135ce527fc037f8547b27f4fb037d6
Roberto Pacheco <Robertoaruajo230@hotmail.com>
2022-09-25 13:38:52 -0300

    Criando classe de Cliente

---
+++ entities/Cliente.class.php
@@ -0,0 +1,13 @@
+<?php
+    class Cliente extends Usuario {
+        private $credito = 0;
+
+        public function getCredito(){
+            return $this->credito;
+        }
+
+        public function adicionarCredito($quantidade){
+            $this->credito += $quantidade;
+        }
+    }
+?>
