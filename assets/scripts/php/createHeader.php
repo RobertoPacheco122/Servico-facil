4f7cc77d29a84f4833e2afdf1dcd477e3e8dfc98
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-09-26 13:32:01 -0300

    Adicionando o link de Serviços no header

--- assets/scripts/php/createHeader.php
+++ assets/scripts/php/createHeader.php
@@ -4,11 +4,14 @@
             <header class='header'>
                 <div class='header--container container-3'>
                     <div class='header--container--logo'>
-                        <img src='./assets/img/pacote.png' alt='Logo Serviço Fácil' class='header__logo'>
+                        <a href='servicos.php' class='footer__link'>
+                            <img src='./assets/img/pacote.png' alt='Logo Serviço Fácil' class='header__logo'>
+                        </a>
                     </div>
                     <div class='header--container--content'>
                         <ul class='header__list'>
                             <li class='header__item'><a href='error.php' class='header__link'>Início</a></li>
+                            <li class='header__item'><a href='servicos.php' class='header__link'>Serviços</a></li>
                             <li class='header__item'><a href='registrar.php' class='header__link'>Registrar-se</a></li>
                             <li class='header__item'><a href='logar.php' class='header__link'>Entrar</a></li>
                         </ul>
