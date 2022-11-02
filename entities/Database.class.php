55aa22c85d8105853d00e0c2cf71e285a8c8fce5
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-11-02 19:26:45 -0300

    Adicionando a funcionalidade de inserir cliente no BD na entidade Database

--- entities/Database.class.php
+++ entities/Database.class.php
@@ -6,5 +6,18 @@
 
             return $database;
         }
+
+        public static function addClient($client){
+            $db = Database::getConnection();
+            $statement = $db->prepare("INSERT INTO Clientes (nome, sobrenome, email, senha, credito) VALUES (:nome, :sobrenome, :email, :senha, :credito)");
+            $statement->bindParam(":nome", $client->getNome(), PDO::PARAM_STR);
+            $statement->bindParam(":sobrenome", $client->getSobrenome(), PDO::PARAM_STR);
+            $statement->bindParam(":email", $client->getEmail(), PDO::PARAM_STR);
+            $statement->bindParam(":senha", $client->getSenha(), PDO::PARAM_STR);
+            $statement->bindParam(":credito", $client->getCredito(), PDO::PARAM_INT);
+            $status = $statement->execute();
+
+            return $status;
+        }
     }
 ?>
