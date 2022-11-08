76b24085712da39deefaab67abd0d100f9d0d73e
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-11-07 23:12:24 -0300

    Criando lógica para trocar entre os formulários de cadastro em registrar.php no arquivo index.js

--- assets/scripts/js/index.js
+++ assets/scripts/js/index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const buttonClient = document.getElementById("client--button");
+const buttonProvider = document.getElementById("provider--button");
+const formClient = document.getElementById("register__form--client");
+const formProvider = document.getElementById("register__form--provider");
+
+function addSwitchEvent(button, form1, form2) {
+    button.addEventListener("click", () => {
+        if(!(form1.classList.contains("hidden"))) return;
+        form1.classList.toggle("hidden");
+        form2.classList.toggle("hidden");
+    });
+}
+
+addSwitchEvent(buttonClient, formClient, formProvider);
+addSwitchEvent(buttonProvider, formProvider, formClient);
