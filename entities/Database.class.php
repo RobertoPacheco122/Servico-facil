01af35954bd9d2cf8db4ee606981b0b7f4bf11ef
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-11-07 23:08:29 -0300

    Adicionando as funcionalidades de getUserId, getUserEmail, getUserPassword, getUserName e getUserLastname na classe Database

--- entities/Database.class.php
+++ entities/Database.class.php
@@ -1,23 +1,67 @@
 <?php
     abstract class Database {
-        public static function getConnection(){
+        private static function getConnection(){
             $database = new PDO("mysql:host=localhost;dbname=servico_facil;charset=utf8mb4","root","");
             $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             return $database;
         }
 
-        public static function addClient($client){
+        public static function addClient($user){
             $db = Database::getConnection();
             $statement = $db->prepare("INSERT INTO Clientes (nome, sobrenome, email, senha, credito) VALUES (:nome, :sobrenome, :email, :senha, :credito)");
-            $statement->bindParam(":nome", $client->getNome(), PDO::PARAM_STR);
-            $statement->bindParam(":sobrenome", $client->getSobrenome(), PDO::PARAM_STR);
-            $statement->bindParam(":email", $client->getEmail(), PDO::PARAM_STR);
-            $statement->bindParam(":senha", $client->getSenha(), PDO::PARAM_STR);
-            $statement->bindParam(":credito", $client->getCredito(), PDO::PARAM_INT);
-            $status = $statement->execute();
+            $status = $statement->execute([":nome", $user->getNome(), ":sobrenome", $user->getSobrenome(), ":email", $user->getEmail(), ":senha", $user->getSenha(), ":credito", $user->getCredito()]);
 
             return $status;
         }
+
+        public static function getUserId($email, $senha){
+            $database = Database::getConnection();
+            $statement = $database->prepare("SELECT id_cliente FROM Clientes WHERE email = :email AND senha = :senha");
+            $status = $statement->execute([":email" => $email, ":senha" => $senha]);
+            $clientId = $statement->fetch();
+
+            return $clientId[0];
+        }
+
+        public static function getUserEmail($userId){
+            $database = Database::getConnection();
+            $statement = $database->prepare("SELECT email FROM Clientes WHERE id_cliente = :id_cliente");
+            $status = $statement->execute([":id_cliente" => $userId]);
+            $clientEmail = $statement->fetch();
+
+            return $clientEmail[0];
+        }
+
+        public static function getUserPassword($userId){
+            $database = Database::getConnection();
+            $statement = $database->prepare("SELECT senha FROM Clientes WHERE id_cliente = :id_cliente");
+            $status = $statement->execute([":id_cliente" => $userId]);
+            $clientPassword = $statement->fetch();
+
+            return $clientPassword[0];
+        }
+
+        public static function getUserName($userId){
+            $database = Database::getConnection();
+            $statement = $database->prepare("SELECT nome FROM Clientes WHERE id_cliente = :id_cliente");
+            $status = $statement->execute([":id_cliente" => $userId]);
+            $clientName = $statement->fetch();
+
+            return $clientName[0];
+        }
+
+        public static function getUserLastname($userId){
+            $database = Database::getConnection();
+            $statement = $database->prepare("SELECT sobrenome FROM Clientes WHERE id_cliente = :id_cliente");
+            $status = $statement->execute([":id_cliente" => $userId]);
+            $clientLastname = $statement->fetch();
+
+            return $clientLastname[0];
+        }
+
+        //public static function authenticateEmail($email){}
+        //public static function authenticatePassword($password){}
+        //public static function authenticateUser($email, $senha){}
     }
 ?>
