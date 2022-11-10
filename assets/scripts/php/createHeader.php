aefac002ffe8a7604eb1c125e43fe67728cfb942
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2022-11-10 16:37:48 -0300

    Adicionando o nome do usuário da sesão no header das páginas

--- assets/scripts/php/createHeader.php
+++ assets/scripts/php/createHeader.php
@@ -1,5 +1,15 @@
 <?php
+    include("./assets/scripts/php/checkSession.php");
+
     function createHeader(){
+        $sessao = checkSession();
+
+        if ($sessao != null) { 
+            $listItem = "<li class='header__item'>Olá, $sessao</li>";
+        } else {
+            $listItem = "<li class='header__item'> Olá, faça o <a href='logar.php'class='header__link'>Login</a> ou <a href='registrar.php' class='header__link'>Registre-se</a></li>";
+        }
+
         return "
             <header class='header'>
                 <div class='header--container container-3'>
@@ -13,8 +23,7 @@
                             <li class='header__item'><a href='error.php' class='header__link'>Início</a></li>
                             <li class='header__item'><a href='servicos.php' class='header__link'>Serviços</a></li>
                             <li class='header__item'><a href='servicos-status.php' class='header__link'>Status</a></li>
-                            <li class='header__item'><a href='registrar.php' class='header__link'>Registrar-se</a></li>
-                            <li class='header__item'><a href='logar.php' class='header__link'>Entrar</a></li>
+                            $listItem
                         </ul>
                     </div>
                 </div>
