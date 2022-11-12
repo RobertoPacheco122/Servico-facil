66b4df60a7e6ab6d068e2c7f67dd1f654528865e
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-11-12 14:03:39 -0300

    Alterando o include de Usuario para require_once

--- entities/Prestador.class.php
+++ entities/Prestador.class.php
@@ -1,8 +1,13 @@
 <?php
-    include("./entities/Usuario.class.php");
+    require_once("./entities/Usuario.class.php");
 
     class Prestador extends Usuario {
         private $tipoServico;
+
+        public function __construct($nome, $sobrenome, $email, $senha, $tipoUsuario, $tipoServico){
+            parent::__construct($nome, $sobrenome, $email, $senha, $tipoUsuario);
+            $this->tipoServico = $tipoServico;
+        }
 
         public function getTipoServico() {
             return $this->tipoServico;
