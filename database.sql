0958858564111c35aa48d47cef2f68aec3bbf494
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2022-11-16 16:39:58 -0300

    Criando tabela unificada de Usuários

--- database.sql
+++ database.sql
@@ -1,25 +1,31 @@
-create table Clientes (
-	id_cliente int auto_increment primary key,
-	nome varchar(15),
-	sobrenome varchar(15),
-	email varchar (50) primary key,
-	senha varchar(20),
-    credito float(10),
-	tipo_usuario int not null
+create table Usuarios (
+	id_usuario int auto_increment,
+	nome varchar(20),
+	sobrenome varchar(20),
+	email varchar(45),
+	senha varchar(25),
+	credito float(10),
+	tipo_servico varchar(25),
+	tipo_usuario int,
+	primary key (id_usuario, email)
 );
 
-create table Prestadores (
-	id_prestador int auto_increment primary key,
-	nome varchar(15),
-	sobrenome varchar(15),
-	email varchar (50) primary key,
-	senha varchar(20),
-    tipo_servico varchar(30),
-	tipo_usuario int not null
-);
+/*
+create table Solicitacoes (
+	num_pedido int auto_increment primary key,
+	id_usuario_prestador int,
+	id_usuario_tomador int,
+	condicao varchar(30),
+	servico varchar(40),
+	tipo_servico varchar(40),
+	data_servico varchar(10),
+	observacao varchar(120),
+	horario varchar(5),
+	preco float(10)
+)
+*/
 
-insert into Clientes (nome, sobrenome, email, senha, credito, tipo_usuario) values ("Roberto", "Pacheco", "cliente1@teste.com.br", "senha123", 40.00, 1);
-insert into Clientes (nome, sobrenome, email, senha, credito) values ("Pedro", "Landi", "cliente2@teste.com.br", "senha321", 0, 1);
-
-insert into Prestadores (nome, sobrenome, email, senha) values ("Thiago", "Policarpo", "prestador1@teste.com.br", "senha456", "Bombeiro", 2);
-insert into Prestadores (nome, sobrenome, email, senha) values ("Jair", "Bolsonaro", "prestador2@teste.com.br", "senha654", "Pescador", 2);
+insert into Usuarios (nome, sobrenome, email, senha, credito, tipo_usuario) values ("Roberto", "Pacheco", "cliente1@teste.com.br", "senha123", 40.00, 1);
+insert into Usuarios (nome, sobrenome, email, senha, credito, tipo_usuario) values ("Pedro", "Landi", "cliente2@teste.com.br", "senha321", 0, 1);
+insert into Usuarios (nome, sobrenome, email, senha, tipo_servico, tipo_usuario) values ("Thiago", "Policarpo", "prestador1@teste.com.br", "senha456", "Bombeiro Hidráulico", 2);
+insert into Usuarios (nome, sobrenome, email, senha, tipo_servico, tipo_usuario) values ("Jair", "Bolsonaro", "prestador2@teste.com.br", "senha654", "Eletricista", 2);
