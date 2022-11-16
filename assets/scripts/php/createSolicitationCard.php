dcd27f36cc0c7a7f6d1c150a0148004cf00515e1
Roberto Pacheco <roberto.2120478300031@faeterj-rio.edu.br>
2022-11-16 20:27:20 -0300

    Criando script php para gerar o card de solicitações

---
+++ assets/scripts/php/createSolicitationCard.php
@@ -0,0 +1,28 @@
+<?php
+
+function createSolicitationCard($solicitation){
+    echo "
+    <div class='status--container--card'>
+        <header class='status__header'>
+            <div class='status--container--header'>
+                <p class='status__number'>Pedido: " . $solicitation['num_pedido'] . "</p>
+                <br>
+                <p class='status__service'>Status: " . $solicitation['condicao'] . "</p> 
+             </div>
+            <h1 class='status__title--card'> " . $solicitation['tipo_servico'] . "</h1>
+            <p class='status__text'>Serviço: " . $solicitation['servico'] . "</p>
+            <p class='status__text'>Preço: R$ " . $solicitation['preco'] . "</p>
+            <p class='status__text'>Data: " . $solicitation['data_servico'] . "</p>
+            <p class='status__text'>Horário: " . $solicitation['horario'] . "</p>
+        </header>
+        <div class='status--container--content'>
+            <div class='status--container--obs'>
+                <p class='status__obs'>Observações: " . $solicitation['observacao'] . "</p>
+            </div>
+            <div class='status--container--button'>
+                <!-- <a href='error.php' class='status__button'>Cancelar serviço</a> -->
+            </div>
+        </div>
+    </div>
+    ";
+}
