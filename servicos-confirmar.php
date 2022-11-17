bfe790aa3a87829e02f53fa8d89fd7ce69d39c4d
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2022-11-17 14:06:10 -0300

    Imprimindo os dados da última solicitação do usuário

--- servicos-confirmar.php
+++ servicos-confirmar.php
@@ -3,7 +3,12 @@
     include "entities/Servico.class.php";
     include "assets/scripts/php/createHeader.php";
     include "assets/scripts/php/createFooter.php";
+    include_once("./entities/Database.class.php");
 
+    session_start();
+
+    $solicitation = Database::getLastSolicitation($_SESSION['userId']);
+    $userCredit = Database::getUserCredit($_SESSION['userId']);
 ?>
 
 <!DOCTYPE html>
@@ -26,20 +31,26 @@
         <main class="main">
             <section class="confirm">
                 <div class="confirm--container container-3">
-                    <div class="confirm--container--image">
-                        <img src="./assets/img/confirmar-servico1.png" alt="" class="confirm__img">
-                        <h1 class="confirm__title">Falta pouco!</h1>
-                        <p class="confirm__text">Revise os dados antes de confirmar a solicitação.</p>
-                    </div>
-                    <div class="confirm--container--content">
-                        <h2 class="confirm__service"><span class="confirm__data">Tipo de serviço:</span> Eletricista</h2>
-                        <p class="confirm__service"><span class="confirm__data">Serviço:</span> Fazer gato de energia</p>
-                        <p class="confirm__service"><span class="confirm__data">Preço do serviço:</span> R$85.59</p>
-                        <p class="confirm__service"><span class="confirm__data">Crédito :</span> R$10.00</p>
-                        <p class="confirm__service"><span class="confirm__data">Valor total:</span> R$75.59</p>
 
-                        <a href="confirmar-pedido.php" class="confirm__link">Confirmar pedido</a>
-                    </div>
+                <?php 
+                    echo "
+                        <div class='confirm--container--image'>
+                            <img src='./assets/img/confirmar-servico1.png' alt='' class='confirm__img'>
+                            <h1 class='confirm__title'>Falta pouco!</h1>
+                            <p class='confirm__text'>Revise os dados antes de confirmar a solicitação.</p>
+                        </div>
+                        <div class='confirm--container--content'>
+                            <p class='confirm__service'><span class='confirm__data'>Serviço:</span> " . $solicitation['tipo_servico'] . "</p>
+                            <h2 class='confirm__service'><span class='confirm__data'>Tipo de serviço:</span> " . $solicitation['servico'] . " </h2>
+                            <p class='confirm__service'><span class='confirm__data'>Preço:</span> R$" . $solicitation['preco'] . "</p>
+                            <p class='confirm__service'><span class='confirm__data'>Crédito :</span> R$" . $userCredit . "</p>
+                            <p class='confirm__service'><span class='confirm__data'>Valor final:</span> R$" . $solicitation['preco'] - $userCredit . "</p>
+        
+                            <a href='solicitacoes.php' class='confirm__link'>Confirmar pedido</a>
+                        </div>
+                    "
+                ?>
+
                 </div>
             </section>
         </main>
