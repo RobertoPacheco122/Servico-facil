abf5c7073af43e522ce6945cdd8de82516e7b151
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-11-18 17:36:51 -0300

    Excluindo arquivos de scripts específicos que se tornaram obsoletos pelo miLibrary.php

--- assets/scripts/php/checkSession.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function checkSession(){
-    session_start();
-
-    if(isset($_SESSION["user"])) return $_SESSION["user"];
-}
--- assets/scripts/php/showSerivce.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function showServices($services) {
-        for ($i = 0; $i < count($services); $i++) {
-            echo "<option value='$i'>".$services[$i]->getServico()."</option>";
-        }
-    }
-?>
