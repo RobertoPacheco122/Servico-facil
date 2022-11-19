a888c034fb45ef8889fbbdfcca64a8bd56d16771
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-11-18 23:34:52 -0300

    Corrigindo os erros na função createSolicitationCard causados pelas alterações na estrutura do DB

--- assets/scripts/php/myLibrary.php
+++ assets/scripts/php/myLibrary.php
@@ -123,7 +123,9 @@ function createSolicitationCard($solicitation){
     $providerName = Database::getUserName($solicitation['id_usuario_prestador']);
     $providerLastname = Database::getUserLastname($solicitation['id_usuario_prestador']);
     $providerEmail = Database::getUserEmail($solicitation['id_usuario_prestador']);
-
+    $price = Database::getTypeServicePrice($solicitation['id_tipo_servico']);
+    $typeServiceName = Database::getTypeServiceName($solicitation['id_tipo_servico']);
+    $serviceName = Database::getServiceName($solicitation['id_servico']);
     echo "
     <div class='status--container--card'>
         <header class='status__header'>
@@ -132,16 +134,18 @@ function createSolicitationCard($solicitation){
                 <br>
                 <p class='status__service'>Status: " . $solicitation['condicao'] . "</p> 
              </div>
-            <h1 class='status__title--card'> " . $solicitation['tipo_servico'] . "</h1>
-            <p class='status__text'>Serviço: " . $solicitation['servico'] . "</p>
+            <h1 class='status__title--card'> " . $serviceName . "</h1>
+            <p class='status__text'>Tipo Serviço: " . $typeServiceName . "</p>
             <p class='status__text'>Data: " . $solicitation['data_servico'] . "</p>
             <p class='status__text'>Horário: " . $solicitation['horario'] . "</p>
+            <p class='status__text'>Preço: R$" . $price . "</p>
             <p class='status__text'>Prestador: " . $providerName . " " . $providerLastname ."</p>
             <p class='status__text'>Email: " . $providerEmail . "</p>
         </header>
         <div class='status--container--content'>
             <div class='status--container--obs'>
-                <p class='status__obs'>Observações: " . $solicitation['observacao'] . "</p>
+                <p class='status__obs'>Observações: </p>
+                <p class='status__text'>" . $solicitation['observacao'] . "</p>
             </div>
             <div class='status--container--button'>
                 <a href='error.php' class='status__button'>Cancelar serviço</a>
