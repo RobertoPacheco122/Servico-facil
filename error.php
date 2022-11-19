7f650db009673b127dc4c20ec9aab27049ba4852
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-11-19 17:27:34 -0300

    Alterando a mensagem de erro

--- error.php
+++ error.php
@@ -8,30 +8,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Serviço Fácil | Em construção</title>
+    <title>Serviço Fácil | Erro</title>
 
     <link rel="stylesheet" href="./assets/css/basic-style.css">
 
     <link rel="stylesheet" href="./assets/css/error/style.css">
 </head>
 <body>
-    <?php
-        echo createHeader();
-    ?>
+    <?php createHeader(); ?>
 
     <main class="main">
         <section class="error">
             <div class="error--container container-3">
                 <img src="./assets/img/construcao.png" alt="Parede de tijolos" class="error__img">
                 <h1 class="error__title">Desculpe pelo transtorno!</h1>
-                <p class="error__text">Esta página se encontra em construção. Retorne novamente no futuro.</p>
+                <p class="error__text">Algo deu errado com a sua requisição. Tente novamente mais tarde.</p>
                 <a href="./servicos.php" class="error__link">Ir a página de Serviços</a>
             </div>
         </section>
     </main>
 
-    <?php
-        echo createFooter();
-    ?>
+    <?php createFooter(); ?>
 </body>
 </html>
