fb5a846d01258c1c1583242948fd041ae44067f3
Roberto Pacheco <robertoaraujo230@hotmail.com>
2022-11-24 22:05:22 -0300

    Passando como parâmetro o preço final da solicitação para classe Solicitation

--- createSolicitation.php
+++ createSolicitation.php
@@ -11,8 +11,7 @@ $hour = $_POST["hour"];
 $finalPrice = $_POST["final-price"];
 
 $solicitation = new Solicitacao(
-    Database::getServiceIdFromTs($typeServiceId), $typeServiceId, 3, $_SESSION["userId"], "Aguardando início", $date, $hour 
-);
+    Database::getServiceIdFromTs($typeServiceId), $typeServiceId, 3, $_SESSION["userId"], "Aguardando início", $date, $hour, $finalPrice);
 
 $insertStatus = Database::insertSolicitation($solicitation);
 
